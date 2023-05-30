1997b9d947e6231bc7e3e48a1a4fb6506aff4309
didtlrhks <105967852+didtlrhks@users.noreply.github.com>
2023-05-30 17:51:49 +0900

    file 추출 하여서 터미널에 보여주기 성공

--- ftpclient.c
+++ ftpclient.c
@@ -4,16 +4,16 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
 
 #define BUF_SIZE 1024
 
 int main()
 {
-    int sock;
+    int sock, data_sock;
     char message[BUF_SIZE];
-    int str_len;
-    struct sockaddr_in serv_adr;
+    int str_len, data_port;
+    struct sockaddr_in serv_adr, data_adr;
+    int h1, h2, h3, h4, p1, p2;
 
     // Control Connection 생성
     sock=socket(PF_INET, SOCK_STREAM, 0);   
@@ -48,8 +48,24 @@ int main()
     read(sock, message, BUF_SIZE - 1);
     printf("Response: %s\n", message);
 
-    // TODO: PASV 응답에서 데이터 연결 포트 추출
-    // NOTE: 실제 프로그램에서는 이 부분을 구현해야 합니다.
+    // PASV 응답에서 데이터 연결 포트 추출
+    sscanf(message, "227 Entering Passive Mode (%d,%d,%d,%d,%d,%d)", &h1, &h2, &h3, &h4, &p1, &p2);
+    data_port = p1 * 256 + p2;
+
+    // Data Connection 생성
+    data_sock=socket(PF_INET, SOCK_STREAM, 0);   
+    if(data_sock==-1)
+        printf("socket() error");
+
+    memset(&data_adr, 0, sizeof(data_adr));
+    data_adr.sin_family=AF_INET;
+    data_adr.sin_addr.s_addr=inet_addr("223.194.7.95");
+    data_adr.sin_port=htons(data_port);
+
+    if(connect(data_sock, (struct sockaddr*)&data_adr, sizeof(data_adr))==-1)
+        printf("connect() error!");
+    else
+        printf("Data Connected...\n");
 
     // RETR 명령 전송
     sprintf(message, "RETR %s\r\n", "welcome.txt");
@@ -57,9 +73,13 @@ int main()
     read(sock, message, BUF_SIZE - 1);
     printf("Response: %s\n", message);
 
-    // TODO: 데이터 연결로부터 파일 내용 수신
-    // NOTE: 실제 프로그램에서는 이 부분을 구현해야 합니다.
+    // 데이터 연결로부터 파일 내용 수신
+    while ((str_len = read(data_sock, message, BUF_SIZE - 1)) != 0) {
+        message[str_len] = 0;  // 널 문자를 추가하여 올바른 문자열로 만듭니다.
+        printf("%s", message);
+    }
 
     close(sock);
+    close(data_sock);
     return 0;
 }
